6eb36e6894daa7ebc90dc5314cbcdb70c45aa766
Paula-18 <paunahappy@gmail.com>
2019-09-06 16:48:59 -0700

    cambio3

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 // Hola (Paula)
 
 const button = document.querySelectorAll('button');
+
+const playnote = event =>{
+    const button = event.target;
+    const note = button.dataset.note;
+    const audioId = `audio${note}`;
+    //console.log(audioId)
+}
