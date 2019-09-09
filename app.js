0f4590941ebcfde86beb6470995c14c9ef0ac38d
Paula-18 <paunahappy@gmail.com>
2019-09-09 09:15:43 -0700

    sonido en js

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-// Hola (Paula)
-
-const button = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('button');
 
 const playnote = event =>{
     const button = event.target;
     const note = button.dataset.note;
     const audioId = `audio${note}`;
-    //console.log(audioId)
     const audio = document.getElementById(audioId);
     console.log(audio);
     audio.pause();
     audio.currentTime = 0;
-    audio.play(); 
+    audio.play();
 }
+
+buttons.forEach(
+    button => button.addEventListener('click', playnote)
+);
+
+const keyNoteDown = event =>{
+    const key = event.key;
+    console.log(key);
+    const button = document.querySelector(`button[data-key="${key}"]`)
+    if(button) button.click(); 
+}
+
+document.addEventListener('keydown', keyNoteDown);
